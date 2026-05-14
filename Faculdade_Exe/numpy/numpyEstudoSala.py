9a0e4f4d7aabea556c6b7d2ccebffc5d7aa1b244
Gabriel Carvalho Lorenssetti <gabriel.lorenssetti@softplan.com.br>
2026-05-14 10:50:21 -0300

    update aula numpy

--- Faculdade_Exe/numpy/numpyEstudoSala.py
+++ Faculdade_Exe/numpy/numpyEstudoSala.py
@@ -1,9 +1,22 @@
+from matplotlib import pyplot as plt
 import numpy as np
+import cv2
 
-list1 = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
+img = cv2.imread("banner.png")
+img = np.array(img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-meu
+branco = np.full(img.shape, [255,255,255])
+m3 = np.hstack((img,branco))
+m4 = np.hstack((branco,img))
+m5 = np.vstack((m3,m4))
+
+plt.imshow(m5)
+plt.show()
+
+lista = ['joao','maria','jose']
+dict1 = {
+    'ch1' : lista[0],
+    'ch2' : lista[1],
+    'ch3' : lista[2]
+}
